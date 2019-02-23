b1f047777f55245632a8f6ae48d8dfbd47a3c52a
Krishna Kumar Dey <krishnakumar.d16@iiits.in>
2019-02-23 19:49:47 +0530

    Added mongo Engine

--- socialcops/__init__.py
+++ socialcops/__init__.py
@@ -1,17 +1,20 @@
 # flask imports go here
 from flask import Flask
 from flask_mongoalchemy import MongoAlchemy
+from flask_mongoengine import MongoEngine
 import os
 
 # declaring the app
 app = Flask(__name__)
-app.config['MONGOALCHEMY_DATABASE'] = 'socialcops'
-
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'socialcops',
+    'host': 'mongodb://localhost/database_name'
+}
 UPLOAD_FOLDER = '/path/to/the/uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # create db
-db = MongoAlchemy(app)
+db = MongoEngine(app)
 # get the base dir
 base_dir = os.path.dirname(os.path.abspath(__file__))
 app.config.from_pyfile(os.path.join(base_dir, '../app.cfg'))
