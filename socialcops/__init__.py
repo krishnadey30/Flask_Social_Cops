8fa56b75a3aabf1ca32b2ef6fc075e61af324818
krishnadey30 <krishnakumardey.dey@gmail.com>
2019-02-23 14:37:02 +0000

    updated in upload

--- socialcops/__init__.py
+++ socialcops/__init__.py
@@ -7,18 +7,17 @@ import os
 app = Flask(__name__)
 # get the base dir
 base_dir = os.path.dirname(os.path.abspath(__file__))
-app.config.from_pyfile(os.path.join(base_dir, '../app.cfg'))
+#app.config.from_pyfile(os.path.join(base_dir, '../app.cfg'))
 app.config['MONGODB_SETTINGS'] = {
     'db': 'socialcops',
     'host': 'mongodb://localhost/database_name'
 }
-UPLOAD_FOLDER = os.path.join(base_dir, '/media/')
+UPLOAD_FOLDER = os.path.join(base_dir, 'media/')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-print(UPLOAD_FOLDER)
 
 # create db
 db = MongoEngine(app)
-
+app.config.from_pyfile(os.path.join(base_dir, '../app.cfg'))
 # importing views
 import socialcops.views
 
